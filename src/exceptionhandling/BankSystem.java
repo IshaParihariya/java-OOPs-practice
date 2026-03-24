f2848bd8b0259e27cd0b8304d305fa79938deda9
IshaParihariya <ishh1036@gmail.com>
2026-03-24 11:04:35 +0530

    update in Bank System

--- src/exceptionhandling/BankSystem.java
+++ src/exceptionhandling/BankSystem.java
@@ -1,9 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package exceptionhandling;
 // for insufficient exception handling
+
+import java.util.Scanner;
+
 class ieh extends Exception
 {
     // constructor
@@ -30,6 +30,62 @@ public class BankSystem
     {
         balance+=amount;
          System.out.println("DEPOSIT SUCCESSFUL");
+    }
+    
+     // main class
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        while(true)
+        {
+            System.out.println("\n1. Deposition \n2. Withdraw \n3. Check balance \n4. Exit ");
+            System.out.println("Enter the choice : ");
+            
+            // try and catch blocks
+            try
+            {
+                int choice=sc.nextInt();
+                // switch case
+                switch(choice)
+                {
+                    case 1:
+                        System.out.print("enter amount :");
+                        double dep=sc.nextDouble();
+                        deposit(dep);
+                        break;
+                     
+                        case 2:
+                        System.out.print("enter amount :");
+                        double amt=sc.nextDouble();
+                        withdraw(amt);
+                        break;
+                        
+                        case 3:
+                            System.out.println("Balance : "+balance);
+                            break;
+                        
+                        case 4:
+                            System.out.println("THANK YOU!!!");
+                            
+                        default :
+                            System.out.println("INVALID CHOICE");
+                            break;
+                }
+            }
+            catch(ieh e)
+            {
+                e.printStackTrace();
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+            // finally
+            finally
+            {
+                System.out.println("Transaction processed");
+            }
+        }
     }
     }
 

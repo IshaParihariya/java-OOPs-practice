4cabc13c1499b10cec7e5dd7f667d2dd9e2717c3
IshaParihariya <ishh1036@gmail.com>
2026-04-01 23:04:24 +0530

    Add an interface example for revision

---
+++ src/interfaces/Example.java
@@ -0,0 +1,31 @@
+
+package interfaces;
+
+interface Animal {
+    void sound();   // abstract method
+}
+
+class Dog implements Animal {
+    public void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Cat implements Animal {
+    public void sound() {
+        System.out.println("Cat meows");
+    }
+}
+
+public class Example
+{
+    public static void main(String[] args)
+    {
+                Animal a1 = new Dog();
+        Animal a2 = new Cat();
+
+        a1.sound();
+        a2.sound();
+
+    }
+}
